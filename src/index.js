a9b8096e5ebb844e29d60944d641029f615d9ccb
subifei123 <17621070228@163.com>
2019-05-28 17:24:33 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
+    
+ReactDOM.render(<App/>, document.getElementById('root'));
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'));
+
